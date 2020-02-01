c49938167cea135837289bf5db35130348bda733
Tyler <tegamb@gmail.com>
2020-01-31 23:24:37 -0800

    initial commit

---
+++ db/migrate/20200201072041_create_cars.rb
@@ -0,0 +1,14 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :string
+      t.string :model
+      t.string :string
+      t.string :year
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
